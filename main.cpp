33de19c2da9bf1858d33187a307c7d07a03c3fc7
iNJUSTiCE <injusticenubs@gmail.com>
2021-12-06 22:49:41 +0530

    cleaned_main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ hashing(str);
     std::cout << "|--This program is made by a username \"NOTRAJAT\" @github---------------|" << std::endl;
     std::cout << "|--Special thanks to \"learnmeabitcoin\" @YouTube------------------------|" << std::endl;
     std::cout << "|--for the explaination video link:https://youtu.be/f9EbD6iY9zI--------|" << std::endl;
+    std::cout << "|--And is licensed with GPL V2.0---------------------------------------|" << std::endl;
     std::cout << "|______________________________________________________________________|" << std::endl;
 }
 	
-}
+
